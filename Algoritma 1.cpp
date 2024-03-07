551dbfae0572ad16c39c5063fd0f29359934293c
gibranrafii <gibranrafi21@gmail.com>
2024-03-07 11:38:46 +0700

    membuat penjelasan display

--- Algoritma 1.cpp
+++ Algoritma 1.cpp
@@ -26,15 +26,15 @@ void input() {	//procedure untuk input
 }
 
 void display() {			//procedure untuk menampilkan hasil
-	cout << endl;
-	cout << "=================================" << endl;
-	cout << "Element Array yang telah tersusun" << endl;
-	cout << "=================================" << endl;
-	for (int j = 0; j < n; j++) {
-		cout << "Data ke-" << j + 1 << "; ";
-		cout << a[j] << endl;
+	cout << endl;			//output baris kosong
+	cout << "=================================" << endl;	//output ke layar
+	cout << "Element Array yang telah tersusun" << endl;	//output ke layar
+	cout << "=================================" << endl;	//output ke layar
+	for (int j = 0; j < n; j++) {				//looping dengan j dimulai dari 0 hingga n-1
+		cout << "Data ke-" << j + 1 << "; ";	
+		cout << a[j] << endl;					//output ke layar
 	}
-	cout << endl; 
+	cout << endl;								//output baris kosong
 }
 
 void bubbleSortArray() {	//procedure untuk mengurutkan
