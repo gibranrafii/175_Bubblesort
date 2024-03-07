58bc872ac43aebcd57162931a99355d91bea8a19
gibranrafii <gibranrafi21@gmail.com>
2024-03-07 12:16:22 +0700

    mengupdate typo di bagian input

--- Algoritma 1.cpp
+++ Algoritma 1.cpp
@@ -19,8 +19,8 @@ void input() {	//procedure untuk input
 	cout << "Masukkan Elemen Array" << endl;	//output ke layar
 	cout << "=====================" << endl;	//output ke layar
 
-	for (int i = 0; 1 < n; i++) {		//looping dengan i dimulai dari 0 hingga n-1
-		cout << "Data ke-" << (i + 1) << "; ";	//output ke layar
+	for (int i = 0; i < n; i++) {		//looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";	//output ke layar
 		cin >> a[i];		//input dari pengguna
 	}
 }
@@ -39,12 +39,12 @@ void display() {			//procedure untuk menampilkan hasil
 
 void bubbleSortArray() {	//procedure untuk mengurutkan array dengan metode bubble sort
 	int pass = 1;			//step 1
-	for (pass; pass <= n - 1; pass + 1) {		//looping dengan i dimulai dari 1 hingga n-1
+	for (pass; pass <= n - 1; pass ++) {		//looping dengan i dimulai dari 1 hingga n-1
 		for (int j = 0; j <= n - 1 - pass; j++) {	//jika nilai pada a[j] lebih besar dari a[j+1]
 			if (a[j] > a[j + 1]) {					//simpan ke nilai a[j] ke variabel sementara temp
 				int temp = a[j];					//assign nilai a[j+1] ke a[j]
 				a[j] = a[j + 1];					//assign nilai temp ke a[j+1]
-				a[j + 1];
+				a[j + 1] = temp;
 			}
 		}
 	}
